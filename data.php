21308879e139b988ed8dd6a5697f5351183e577d
azhararrozak <azhardrozak2001@upi.edu>
2024-05-10 18:11:41 +0700

    First Commit

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+require_once('menu.php');
+
+$juice = new Menu('JUS');
+$coffee = new Menu('KOPI');
+$curry = new Menu('GULAI');
+$pasta = new Menu('PASTA');
+// Deklarasikan array $menus
+$menus = array($juice, $coffee, $curry, $pasta)
+
+?>
